dd18c510bd96d1d373ce25b9c29cfac83b0a6365
Rodrigo Azevedo <134436718+Rodrig87@users.noreply.github.com>
2023-09-28 10:10:35 -0300

    Update and rename exercicio-7.py to exercicio-7atualizado.py

---
+++ exercicio-7atualizado.py
@@ -0,0 +1,9 @@
+num = int(input("Digite um número: "))
+
+
+if num == 10:
+    print("ESSE NÚMERO É IGUAL A 10!")
+elif num > 10:
+    print("É MAIOR QUE 10!")
+else:
+    print("NÃO É MAIOR QUE 10!")
